75e4c3fae6ed8fd8467be3376c73e040e527c3be
SantrupthiKori <91019890+SantrupthiKori@users.noreply.github.com>
2021-09-28 13:01:16 +0530

    Delete NameAge.py

--- NameAge.py
+++
@@ -1,5 +0,0 @@
-name= (input("Please enter name: "))
-age= int(input("Please enter age: "))
-futureage= (100-age)+2021
-print("Hello", name, "your age is" , age )
-print("Hello", name, "you will turn 100 years after" , futureage )
